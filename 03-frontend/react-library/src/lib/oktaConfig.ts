23fb32b18235208ac6049a34be32f5564d5bd598
koushik <kmalagannavar@gmail.com>
2025-03-02 14:57:06 +0530

    oktaConfig modiefied redirectURI from http to https

--- 03-frontend/react-library/src/lib/oktaConfig.ts
+++ 03-frontend/react-library/src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oak674n2lAza3aQX5d7',
     issuer: 'https://dev-66796873.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:5174/login/callback',
+    redirectUri: 'https://localhost:5174/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
